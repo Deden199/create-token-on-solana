1aa9213ed822617d961f18bfc07b14f1232fd976
Deden199 <92734420+Deden199@users.noreply.github.com>
2025-01-23 22:13:37 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,7 +108,7 @@ export default function HomePage() {
     Create your own SPL token with Solradian. Our tools are sufficient for your primary needs, allowing you to add directly to the pool without unnecessary extras.
   </p>
   <a
-    href="#features"
+    href="https://app.binance.com/uni-qr/cart/19329812857921?isst=1&l=en&r=86987631&uc=web_square_share_link&uco=iBxCMe2QYRNVBYGvj3RgbA&us=copylink"
     className="px-5 md:px-6 py-2 md:py-3 bg-cyan-500 text-sm md:text-lg font-semibold text-white rounded-md shadow-md btn-glow hover:bg-cyan-600 transition"
   >
     Learn More
